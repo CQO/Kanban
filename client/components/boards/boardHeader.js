125ce0cf1f9a8cfa0300fddf37e1be3e0cccdebc
PUGE <my@owo.help>
2016-10-11 10:46:46 +0800

    清理watch

--- client/components/boards/boardHeader.js
+++ client/components/boards/boardHeader.js
@@ -69,7 +69,6 @@ BlazeComponent.extendComponent({
       },
       'click .js-open-board-menu': Popup.open('boardMenu'),
       'click .js-change-visibility': Popup.open('boardChangeVisibility'),
-      'click .js-watch-board': Popup.open('boardChangeWatch'),
       'click .js-open-filter-view'() { Sidebar.setView('filter'); },
       'click .js-filter-reset'(evt) {
         evt.stopPropagation();
