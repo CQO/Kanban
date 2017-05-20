fc6f8c1781d5639dfed23d3798e6f5113e1f5140
蒲鸽 <my@owo.help>
2017-05-21 00:41:07 +0800

    修复i18n

--- client/lib/i18n.js
+++ client/lib/i18n.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 // 我们读取用户所在地语言并把它设置成默认语言
 // 它来自浏览器默认的信息，默认语言为英语
 
@@ -16,6 +16,10 @@ Meteor.startup(() => {
       } else {
         language = navigator.language || navigator.userLanguage;
       }
+    }
+    if (language) {
+      TAPi18n.setLanguage(language);
+      T9n.setLanguage(language);
     }
   });
 });
